5cb962a791763c1cc6fbbf19a1824c74ad0601f3
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2023-08-11 18:51:41 -0700

    Update redis_state.py

--- src/npc_lims/state/redis_state.py
+++ src/npc_lims/state/redis_state.py
@@ -64,7 +64,7 @@ class State(collections.abc.MutableMapping):
         cls.db = redis.Redis(
             host='redis-11357.c1.us-west-2-2.ec2.cloud.redislabs.com',
             port=11357,
-            password=os.environ['REDIS_TOKEN'],
+            password=os.environ['REDIS_DEFAULT_USER_PASSWORD'],
         )
         if cls.db.ping():
             logger.debug('Connected to Redis database: %s', cls.db)
