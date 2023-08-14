a3bb25919edd9b613ced29e29f37d34e57b391d8
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-08-13 17:41:11 -0700

    Add shebang

--- src/npc_lims/scripts/update_session_status.py
+++ src/npc_lims/scripts/update_session_status.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import annotations
 
 import os
@@ -41,10 +43,7 @@ def main() -> None:
             f"Error inserting values into `status` table ob dbhub: {response[1].get('error', 'Unknown error')}"
         )
     else:
-        print(
-            "Successfully updated `status` table on dbhub: https://dbhub.io/svc_neuropix/jobs.db"
-        )
-
-
+        print("Successfully updated `status` table on dbhub: https://dbhub.io/svc_neuropix/jobs.db")
+        
 if __name__ == "__main__":
     main()
