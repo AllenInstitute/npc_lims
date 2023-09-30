b8227bca86229b38ed8e37a840143081ffef3566
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-30 01:03:39 -0700

    Add explicit export of attrs for mypy

--- src/npc_lims/status/__init__.py
+++ src/npc_lims/status/__init__.py
@@ -3,3 +3,9 @@ from npc_lims.status.tracked_sessions import (
     get_session_info,
     get_tracked_sessions,
 )
+
+__all__ = [
+    "SessionInfo",
+    "get_session_info",
+    "get_tracked_sessions",
+]
