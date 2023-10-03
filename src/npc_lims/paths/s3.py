fb8eeaecce726d72082c0e3c641980d0a5de6675
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-03 16:15:06 -0700

    Update getting nwb path to match SessionRecord

--- src/npc_lims/paths/s3.py
+++ src/npc_lims/paths/s3.py
@@ -315,7 +315,7 @@ def get_nwb_file_from_s3(
     session = npc_session.SessionRecord(session)
     root = NWB_REPO
     glob = f"*_{session.replace('-', '')}.nwb"
-    result = next(root.glob(glob), None)
+    result = next((path for path in root.glob(glob) if session == path.stem), None)
     if not result:
         raise FileNotFoundError(f"No NWB file found at {root}/{glob}")
     return result
