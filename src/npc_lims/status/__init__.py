26e19c1c66983958fa4f1c2825f7e70e93ec47d7
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-04 10:45:29 -0700

    Update fn name and add convenience fns

--- src/npc_lims/status/__init__.py
+++ src/npc_lims/status/__init__.py
@@ -1,4 +1,4 @@
-from npc_lims.status.tracked_sessions import SessionInfo, get_session_info
+from npc_lims.status.tracked_sessions import SessionInfo, get_session_info, get_session_issues, get_session_kwargs
 
 __all__ = [
     "SessionInfo",
