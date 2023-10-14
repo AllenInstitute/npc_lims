91ffeceed9464e3d5db5760056146d8b5239ef49
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-13 19:59:01 -0700

    Add fns for getting excel spreadsheets, convert to sqlite

--- src/npc_lims/metadata/__init__.py
+++ src/npc_lims/metadata/__init__.py
@@ -2,3 +2,4 @@ from npc_lims.metadata.codeocean import *
 from npc_lims.metadata.record_dbs import *
 from npc_lims.metadata.records import *
 from npc_lims.metadata.types import *
+from npc_lims.metadata.spreadsheets import *
