80d88cff6356b6450ae6cea59740dcb7c4acda56
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-11-28 09:35:35 -0800

    Print status after upload

--- src/npc_lims/scripts/update_session_status.py
+++ src/npc_lims/scripts/update_session_status.py
@@ -16,7 +16,8 @@ API_KEY = os.getenv("DBHUB_API_KEY")
 def main() -> None:
     # sync sqlite dbs with xlsx sheets on s3
     npc_lims.update_training_dbs()
-
+    print("Successfully updated training DBs on s3.")
+    
     if not API_KEY:
         print("No API key found. Please set the `DBHUB_API_KEY` environment variable.")
         return
