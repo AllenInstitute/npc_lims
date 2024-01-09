603b4637ee80260f994a3418b09419b4b101521f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-01-09 14:04:06 -0800

    Update cache functions

--- src/npc_lims/paths/cache.py
+++ src/npc_lims/paths/cache.py
@@ -15,28 +15,35 @@ CACHE_ROOT = S3_SCRATCH_ROOT / "session-caches"
 NWBComponentStr: TypeAlias = Literal[
     "session",
     "subject",
-    "units",
     "epochs",
-    "intervals",
+    "trials",
+    "performance",
+    "vis_rf_mapping",
+    "aud_rf_mapping",
+    "optotagging",
     "invalid_times",
-    "electrodes",
-    "electrode_groups",
     "devices",
+    "electrode_groups",
+    "electrodes",
+    "units",
+    #TODO licks, pupil area, xy pos, running speed (zarr?)
 ]
 
-CACHED_FILE_EXTENSIONS: dict[str, str] = dict.fromkeys(typing.get_args(NWBComponentStr), '.parquet')
+CACHED_FILE_EXTENSIONS: dict[str, str] = dict.fromkeys(
+    typing.get_args(NWBComponentStr), ".parquet"
+)
 """Mapping of NWB component name to file extension"""
 
 assert CACHED_FILE_EXTENSIONS.keys() == set(
     typing.get_args(NWBComponentStr)
 ), "CACHED_FILE_EXTENSIONS must have keys for all NWBComponent values"
-assert all(v.startswith(".") for v in CACHED_FILE_EXTENSIONS.values()), (
-    "CACHED_FILE_EXTENSIONS must have values that start with a period"
-)
+assert all(
+    v.startswith(".") for v in CACHED_FILE_EXTENSIONS.values()
+), "CACHED_FILE_EXTENSIONS must have values that start with a period"
 
-def get_cache_ext(nwb_component: NWBComponentStr) -> str:
+def get_cache_file_suffix(nwb_component: NWBComponentStr) -> str:
     """
-    >>> get_cache_ext("session")
+    >>> get_cache_file_suffix("session")
     '.parquet'
     """
     if (ext := CACHED_FILE_EXTENSIONS.get(nwb_component, None)) is None:
@@ -45,9 +52,10 @@ def get_cache_ext(nwb_component: NWBComponentStr) -> str:
         )
     return ext
 
+
 def get_current_cache_version() -> str:
     """
-    >>> (get_cache_path("366122_2023-12-31", "units", "v0.0.0").parent / 'test.txt').touch()
+    >>> (get_cache_path(nwb_component="units", session_id="366122_2023-12-31", version="v0.0.0").parent / 'test.txt').touch()
     >>> v = get_current_cache_version()
     >>> assert v >= 'v0.0.0'
     """
@@ -55,17 +63,30 @@ def get_current_cache_version() -> str:
         raise FileNotFoundError(f"No cache versions found in {CACHE_ROOT}")
     return version_dirs[-1].name
 
+
 def _parse_version(version: str) -> str:
     return f"v{packaging.version.parse(str(version))}"
 
-def _parse_cache_path(session: str | npc_session.SessionRecord, nwb_component: NWBComponentStr, version: str | None = None) -> upath.UPath:
-    session = npc_session.SessionRecord(session)
-    version = _parse_version(version)  if version else get_current_cache_version() 
-    return CACHE_ROOT / version / nwb_component / f"{session}_{nwb_component}{get_cache_ext(nwb_component)}"
- 
-def get_cache_path(
-    session: str | npc_session.SessionRecord,
+
+def _parse_cache_path(
     nwb_component: NWBComponentStr,
+    session_id: str | npc_session.SessionRecord | None = None,
+    version: str | None = None,
+) -> upath.UPath:
+    version = _parse_version(version) if version else get_current_cache_version()
+    d = (
+        CACHE_ROOT
+        / version
+        / nwb_component
+    )
+    if session_id is None:
+        return d
+    return d / f"{npc_session.SessionRecord(session_id)}{get_cache_file_suffix(nwb_component)}"
+
+
+def get_cache_path(
+    nwb_component: NWBComponentStr,
+    session_id: str | npc_session.SessionRecord | None = None,
     version: str | None = None,
     check_exists: bool = False,
 ) -> upath.UPath:
@@ -73,14 +94,18 @@ def get_cache_path(
     If version is not specified, the latest version currently in the cache will be
     used, ie. will point to the most recent version of the file.
 
-    >>> get_cache_path(session="366122_2023-12-31", nwb_component="units", version="1.0.0")
-    S3Path('s3://aind-scratch-data/ben.hardcastle/session-caches/v1.0.0/units/366122_2023-12-31_units.parquet')
+    >>> get_cache_path(nwb_component="units", version="1.0.0")
+    S3Path('s3://aind-scratch-data/ben.hardcastle/session-caches/v1.0.0/units')
+    >>> get_cache_path(nwb_component="units", session_id="366122_2023-12-31", version="1.0.0")
+    S3Path('s3://aind-scratch-data/ben.hardcastle/session-caches/v1.0.0/units/366122_2023-12-31.parquet')
     """
-    session = npc_session.SessionRecord(session)
-    path = _parse_cache_path(session, nwb_component, version) 
+    path = _parse_cache_path(session_id=session_id, nwb_component=nwb_component, version=version)
     if check_exists and not path.exists():
-        raise FileNotFoundError(f"Cache file for {session} {nwb_component} {version} does not exist")
+        raise FileNotFoundError(
+            f"Cache file for {session_id} {nwb_component} {version} does not exist"
+        )
     return path
+
 
 def get_all_cache_paths(
     nwb_component: NWBComponentStr,
@@ -89,16 +114,18 @@ def get_all_cache_paths(
     """
     For a particular NWB component, return cached file paths for all sessions, for
     the latest version (default) or a specific version.
-    
+
     >>> get_all_cache_paths("units", version="0.0.0")
     ()
     """
-    dummy_path = _parse_cache_path("366122_2023-12-31", nwb_component, version)
-    if not dummy_path.parent.exists():
-        raise FileNotFoundError(f"Cache directory for {nwb_component} {version} does not exist")
-    return tuple(
-        path for path in dummy_path.parent.glob(f"*{dummy_path.suffix}")
-    )
+    dir_path = get_cache_path(nwb_component=nwb_component, version=version)
+    if not dir_path.exists():
+        raise FileNotFoundError(
+            f"Cache directory for {nwb_component} {version} does not exist"
+        )
+    return tuple(path for path in dir_path.glob(f"*{get_cache_file_suffix(nwb_component)}"))
+
+
     
 if __name__ == "__main__":
     import doctest
